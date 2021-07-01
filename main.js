5bd8e15441312f8e3f9e2c958290d9a72ba70d3f
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-01 21:29:42 +0700

    4.1-4.3
    Chang image and product then add stock

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots'
-            ,discription: 'As a noun description is a sketch or account of anything in words; a portraiture or representation in language; an enumeration of the essential qualities of a thing or species.'
-            ,image: './assets/images/socks_green.jpg'
+            product:'Shoes'
+            ,image:'./assets/images/socks_green.jpg'
+            ,inStock:true
             ,P_name: 'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }
