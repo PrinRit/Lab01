38215a7f929e350a531798431b32ac47a492e548
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-01 21:20:01 +0700

    3.6
    Addd the link to connect to www.camt.cmu.ac.th

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
             product: 'Boots'
             ,discription: 'As a noun description is a sketch or account of anything in words; a portraiture or representation in language; an enumeration of the essential qualities of a thing or species.'
             ,image: './assets/images/socks_green.jpg'
+            ,P_name: 'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }
 })
