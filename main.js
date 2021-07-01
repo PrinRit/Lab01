0491e76fb72d1d76a25db665c6f21379e4540f1e
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-01 21:08:32 +0700

    3.1-3.5
    Add image and v-bind order

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Boots'
             ,discription: 'As a noun description is a sketch or account of anything in words; a portraiture or representation in language; an enumeration of the essential qualities of a thing or species.'
+            ,image: './assets/images/socks_green.jpg'
         }
     }
 })
