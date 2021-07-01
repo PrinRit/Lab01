07012e2c6cf59559f64d52f2201320c16c9e5f56
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-01 20:58:53 +0700

    2.5
    Add discription

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots'
+            ,discription: 'As a noun description is a sketch or account of anything in words; a portraiture or representation in language; an enumeration of the essential qualities of a thing or species.'
         }
     }
 })
